33bc29826e7d0260817db3940228fa8ff5eaee80
limsaehyun <sh00710000@gmail.com>
2023-05-31 11:48:59 +0900

    fix: rename Main to Intro

--- app/src/main/java/com/saehyun/talktopi_android/IntroActivity.kt
+++ app/src/main/java/com/saehyun/talktopi_android/IntroActivity.kt
@@ -5,10 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_intro)
     }
 
     fun onClickSignUp(view: View) {
