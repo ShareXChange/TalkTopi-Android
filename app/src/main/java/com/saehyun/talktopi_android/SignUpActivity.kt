05624b969786d7dadb135a18e08e83082c12e9ed
limsaehyun <sh00710000@gmail.com>
2023-05-31 11:50:40 +0900

    feat: 회원가입 UI 작업

---
+++ app/src/main/java/com/saehyun/talktopi_android/SignUpActivity.kt
@@ -0,0 +1,63 @@
+package com.saehyun.talktopi_android
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+enum class SignUpStep {
+    ACCOUNT, PASSWORD
+}
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var fragmentContainer: FrameLayout
+    private lateinit var nextButton: Button
+    private var step = SignUpStep.ACCOUNT
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        initView()
+    }
+
+    private fun initView() {
+        fragmentContainer = findViewById(R.id.frame_sign_up)
+        nextButton = findViewById(R.id.btn_sign_up_next)
+
+        replaceFragment(SignUpAccountFragment())
+
+        nextButton.setOnClickListener {
+            updateStep()
+        }
+    }
+
+    private fun updateStep() {
+        when (step) {
+            SignUpStep.ACCOUNT -> {
+                step = SignUpStep.PASSWORD
+                replaceFragment(SignUpPasswordFragment())
+            }
+
+            SignUpStep.PASSWORD -> {
+                navigateToMain()
+            }
+        }
+    }
+
+    private fun navigateToMain() {
+        Intent(this, IntroActivity::class.java).apply {
+            startActivity(this)
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_sign_up, fragment)
+            .commit()
+    }
+}
---
+++ app/src/main/java/com/saehyun/talktopi_android/SignUpPasswordFragment.kt
@@ -0,0 +1,17 @@
+package com.saehyun.talktopi_android
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SignUpPasswordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_sign_up_password, container, false)
+    }
+}
