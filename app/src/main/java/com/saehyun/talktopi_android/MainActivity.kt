9a84b6ff3bd56fff955d6a3a88791d620586e844
limsaehyun <sh00710000@gmail.com>
2023-05-31 11:47:57 +0900

    feat: navigate 로직 구현

--- app/src/main/java/com/saehyun/talktopi_android/MainActivity.kt
+++ app/src/main/java/com/saehyun/talktopi_android/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.saehyun.talktopi_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickSignUp(view: View) {
+        Intent(applicationContext, SignUpActivity::class.java).apply {
+            startActivity(this)
+        }
     }
 }
